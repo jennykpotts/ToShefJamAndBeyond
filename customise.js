cea396ab27bd0d2f97d539c3d1bcf1c83998512e
Alicja Szwałek <ala.szwalek@yahoo.com>
2022-02-12 13:29:08 +0000

    changes

--- customise.js
+++ customise.js
@@ -2,11 +2,13 @@
 
 const canvas = document.getElementById("customiseCanvas");
 const ctx = canvas.getContext("2d");
+var img = new Image();
 
+img.src="starrybackground.jpg";
+img.onload = function chooseSpaceship(){ 
+    ctx.drawImage(img,0,0); 
+};
 
-function chooseSpaceship() {
-    alert("hello");
-}
 
 chooseSpaceship();
 
