0e6f35958bd26563e95161ff69f436c1d5ab67c3
hhayleyy <83404357+hhayleyy@users.noreply.github.com>
2022-02-12 17:31:11 +0000

    Y

---
+++ planet.js
@@ -0,0 +1,32 @@
+class planet{
+    constructor(img,topx,topy,bottomx,bottomy){
+        this.img=img;
+        this.topx=topx;
+        this.topy=topy;
+        this.bottomx=bottomx;
+        this.bottomy=bottomy;
+
+    }
+    get img() {
+        return this.img;
+    }
+
+    get topx() {
+        return this.topx;
+    }
+    get topy(){
+        return this.topy;
+
+    }
+    get bottomx(){
+        return this.bottomx
+    }
+    get bottomy(){
+        return this.bottomy
+    }
+
+    moveplanet(){
+        this.topx+=15;
+        this.bottomx+=15;
+    }
+}
